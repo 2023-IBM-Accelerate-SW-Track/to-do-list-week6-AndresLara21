0a02dbbc1e1f38c090ac479e0bbed6cb3c56c803
Andres L <andreslara1923@gmail.com>
2023-07-15 13:17:13 -0700

    added ability for UI to call and return all Todo Lists

--- src/component/TodoData.js
+++ src/component/TodoData.js
@@ -3,10 +3,11 @@ import Axios from "axios";
 
 
 const ShowTodos = () => {
+  const [todos, setTodos] = useState([]);
   useEffect( () => {
     async function fetchData() {
       try {
-        const red = await Axios.get('http://localhost:8080/get/items');
+        const res = await Axios.get('http://localhost:8080/get/items');
         setTodos(JSON.stringify(res.data));
         console.log(JSON.stringify(res.data));
       } catch (err) {
